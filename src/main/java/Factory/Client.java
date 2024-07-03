aaa1a1911a68dc51f41b4765a4b8befc7f1ca385
Jagan Baskar <jackjagan1995@gmail.com>
2024-07-03 14:13:56 +0530

    Added code for Factory design pattern

--- src/main/java/Factory/Client.java
+++ src/main/java/Factory/Client.java
@@ -8,7 +8,7 @@ import Factory.constants.Platform;
 public class Client {
     public static void main(String[] args) {
         Flutter flutter = new Flutter();
-        UIFactory uiFactory = flutter.getUIFactory(Platform.ANDROI  D );
+        UIFactory uiFactory = flutter.getUIFactory(Platform.ANDROID );
 
         Button button = uiFactory.createButton();
         DropDown dropDown = uiFactory.createDropDown();
